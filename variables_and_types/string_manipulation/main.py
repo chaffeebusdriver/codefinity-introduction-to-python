6a7b10b2156a7081a8ff3ae5cd98aecf00e2746c
chaffeebusdriver <>
2026-02-24 01:52:24 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,9 @@
+dairy1 = "milk"
+dairy2 = "cheese"
+bakery1 = "bread"
+
+message = f"We have dairy and bakery items: {dairy1:bakery1} in aisle {5}"
+print(message)
+
+count = 3
+print = (f"There are {count + 2} total items listed.")
